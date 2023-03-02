34ef1456c1fc35a26c2567820193db67a1fec2bb
Hayato <hayato.314.nico@gmail.com>
2023-03-03 02:49:36 +0900

    デプロイエラー対策　NextApiRequest型を指定

--- pages/api/abc.ts
+++ pages/api/abc.ts
@@ -1,16 +1,17 @@
-import mysql from 'mysql2/promise';
+import { NextApiRequest, NextApiResponse } from "next";
+import mysql from "mysql2/promise";
 
-export default async (req, res) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'house-love',
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "house-love",
   });
-  
+
   // ここでデータベースの操作を行う
-  const [rows, fields] = await connection.execute('SELECT * FROM uesrs');
+  const [rows, fields] = await connection.execute("SELECT * FROM uesrs");
   res.status(200).json(rows);
 
   connection.end();
-}
+};
